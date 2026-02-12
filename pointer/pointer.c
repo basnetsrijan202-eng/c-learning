f080019470e3ad869a98154c9e7bdca6f4c69d7c
Srijan Basnet <basnetsrijan202@gmail.com>
2026-02-12 16:20:01 +0545

    my first array

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,9 +1,12 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int a = 25;
-    int *p =&a;
-    printf("value of a = %d\n",a);
-    printf("asddtess of a = %p\n",p);
-    printf("value using pointer = %d\n" ,*p);
+    int *p = &a;
+
+    printf("value of a = %d\n", a);
+    printf("address of a = %p\n", (void*)p);
+    printf("value using pointer = %d\n", *p);
+
     return 0;
 }
